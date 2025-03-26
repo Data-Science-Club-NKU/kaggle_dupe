7129a186d89c6c506e3bf519c77e5c9db754ef38
Sushantsth21 <144493504+Sushantsth21@users.noreply.github.com>
2025-03-26 13:59:29 -0400

    Update next.config.ts

--- abalone/next.config.ts
+++ abalone/next.config.ts
@@ -1,12 +1,42 @@
 import type { NextConfig } from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      // Ensure CSV files are included in the build output
-      "/api/*": ["./data/train.csv", "./data/test.csv", "./data/Sample_submission.csv"],
-    },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    // Define the file paths to copy
+    const filesToCopy = [
+      './data/train.csv',
+      './data/test.csv',
+      './data/Sample_submission.csv',
+    ];
+
+    // Add a rule to copy the files to the public directory
+    config.module.rules.push({
+      test: /\.(csv)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/files',  // Path in the browser
+            outputPath: `${isServer ? '../' : ''}static/files`, // Path in the output directory
+            name: '[name].[ext]',
+            emitFile: !isServer,
+          },
+        },
+      ],
+    });
+
+    return config;
   },
+  output: 'export', // required for static websites
+  distDir: 'dist', // output directory
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
